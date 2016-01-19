c4fa995436e445ba6e6ea5caa6c30f64ef661a56
Ryan Webb <dzanfox@gmail.com>
2016-01-19 16:21:10 -0600

    Added member variables

---
+++ DeathStar/DeathStar.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TopSecretPlans;
+
+namespace DeathStar
+{
+    public class DeathStar
+    {
+        Mainexhaustport mainExhaustPort;
+        Thermalexhaustport ThermalExhaustPort;
+        TractorBeam tractorBeam;
+        TractorBeamGenerator tractorBeamGenerator;
+        EmergencyRadiationDischarge emergencyRadiationDischarge;
+        MainPowerGenerator mainPowerGenerator;
+        HyperMatterReactor hyperMatterReactor;
+        IonDriveReactor ionDriveReactor;
+        OverBridge overBridge;
+        SuperLaser superLaser;
+        MagneticShielding magneticShielding;
+        HyperDrive hyperDrive;
+        PowerCell powerCell;
+        CentralComputerCore centralComputerCore;
+        HeatExchanger heatExchanger;
+        CoolingSystem coolingSystem;
+        Refinery refinery;
+        FuelCollector fuelCollector;
+        ExteriorQuadaniumsteelouterhull exteriorQuadaniumSteelOuterHull;
+        
+    }
+}
